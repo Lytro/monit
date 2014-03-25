47eb1d1b943fd19d9f6d726d1dc16f8df332c776
Anuj Biyani <abiyani@lytro.com>
2014-03-25 16:40:02 -0700

    don't use an attribute unnecessarily

--- attributes/unicorn.rb
+++ attributes/unicorn.rb
@@ -1,3 +1,2 @@
 default[:monit][:unicorn][:pid_dir] = '/path/to/pids'
-default[:monit][:unicorn][:master_pid] = "#{node[:monit][:unicorn][:master_pid]}/unicorn_master.pid"
 default[:monit][:unicorn][:worker_count] = 1
